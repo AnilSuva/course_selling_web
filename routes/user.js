714530b72829d964202cf49e639cfab84071c597
Anil SUva <suvaanil80@gmail.com>
2026-03-03 22:44:13 +0530

    added sigh in routes in user

--- routes/user.js
+++ routes/user.js
@@ -33,14 +33,40 @@ userRouter.post("/signup", async (req, res) => {
             lastName: lastName
         });
 
-        res.status(202).json({ message: "You are signed Up" }); 
-    } catch(err) {
+        res.status(202).json({ message: "You are signed Up" });
+    } catch (err) {
         res.status(404).json(err);
     }
 })
 
-userRouter.post("/signin", (req, res) => {
+userRouter.post("/signin", async (req, res) => {
+    try {
+        const verifySchema = z.object({
+            email: z.string().max(100).min(3),
+            password: z.string().max(25).min(8)
+        })
 
+        const result = verifySchema.safeParse(req.body);
+
+        if (!result.success) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        const { email, password } = result.data;
+        const user = await userModel.findOne({ email: email });
+
+        if (!user) {
+            return res.status(400).json({ message: "User is not found" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            res.status(200).json({ message: "Signin successfully" });
+        } else {
+            res.json({ message: "Incorrect Password" })
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Server error", error: err.message })
+    }
 })
 
 userRouter.get("/course", (req, res) => {
